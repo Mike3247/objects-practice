16d4eff061e702223e71e33cee5f37e6a6441ea8
Mike Selmani <mike_selmani@hotmail.com>
2022-06-25 07:43:43 -0400

    html add div + button, css add display grid to container div, js restart from constructor functions and dummy myLibrary array and empty addBookToLibrary function

--- book-objects.js
+++ book-objects.js
@@ -9,7 +9,7 @@ const player1 = new Player ('steve', 'X')
 const player2 = new Player ('also steve', 'O')
 player1.sayName() // logs 'steve'
 player2.sayName() // logs 'also steve'
-/////
+//////////////////////////////////////////////////////////////////
 let myLibrary = ["The Hobbit", "Harry Potter", "20 000 lieues"];
 function Book (title, author, pages, read) {
     this.title = title + ""
@@ -20,13 +20,42 @@ function Book (title, author, pages, read) {
         return title + author + pages + read
     }
 }
-const theHobbit = new Book ('The Hobbit ', "J.R.R. Tolkien ", "295 pages ", "not read yet ")
+const theHobbit = new Book ("The Hobbit ", "J.R.R. Tolkien ", "295 pages ", "not read yet ")
 console.log(theHobbit.sayInfo());
 console.log(theHobbit.constructor);
 /////
 function addBookToLibrary() {
+    new Book
+}
+/*
+function myFunction(list){
+    var text = "";
+    var inputs = document.querySelectorAll("input[type=text]");
+    for (var i = 0; i < inputs.length; i++) {
+        text += inputs[i].value;
+    }
+    var li = document.createElement("li");
+    var node = document.createTextNode(text);
+    li.appendChild(node);
+    document.getElementById("list").appendChild(li);
+    */
+/*
+function displayBook (livre) {
+    let div = document.createElement("div");
+    div.style.border = "10px";
+    div.style.bordercolor = "fuschia";
+    div.style.borderstyle = "solid";
+    div.style.width = "50px";
+    div.style.height = "50px";
+    const displayMyLibrary = myLibrary.filter(livre => livre);
+    console.table(displayMyLibrary);
+    return livre;
 
 }
+console.table(livre);
+*/
+/*
 const displayMyLibrary = myLibrary.filter(livre => livre);
-
 console.table(displayMyLibrary);
+const container = document.querySelector("#container");
+container.appendChild(displayBook("div")); */
