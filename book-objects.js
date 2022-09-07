7c5c4b7046bd649a42a8ef453766276b33472aca
Mike Selmani <mike_selmani@hotmail.com>
2022-09-06 20:33:38 -0400

    js, change openForm function to select newBookFormDiv instead of newBookForm, thereby allowing user to openForm more than once with click on newBookButton

--- book-objects.js
+++ book-objects.js
@@ -16,18 +16,6 @@ console.log('end of tests for this page from here on out.');
 
 
 let myLibrary = [/*"The Hobbit", "Harry Potter", "20 000 lieues"*/];
-
-myLibrary.forEach(element => {
-    console.log(element);
-    const container = document.querySelector(".container");
-    function createBookDiv (div) {
-        div = document.createElement("div");
-        div.appendChild(document.createTextNode(element))
-        return div
-    }
-    container.appendChild(createBookDiv("div")); 
-});
-
 function Book (title, author, pages, read) {
     this.title = title + ""
     this.author = author
@@ -43,106 +31,40 @@ console.log(theHobbit.constructor);
 const container = document.querySelector(".container");
 const buttonDiv = document.querySelector(".newBookButton");
 const btn = document.querySelector("button")
-btn.addEventListener("click", () => {
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-};
-});
-
 function openForm() {
     document.getElementById("newBookFormDiv").style.display = "block";
-  };
-
+};
 function closeForm() {
-    document.getElementById("newBookForm").style.display = "none";
-  }
-
-document.getElementById("newBookButton").addEventListener("click", () => {
-    console.log("click-test-achieved")
-    openForm();
-  
-    document.getElementById("closeFormButton").addEventListener("click", () => {
-        console.log("click-test-achieved")
-        let authorName = document.getElementById("author").value;
-        alert(authorName);
-        function addBookToLibrary() { 
-            let input = authorName;
-            alert(`You read ${input}`);
-            myLibrary.push(`${input}`);
-            return{
-                myLibrary : myLibrary
-            }
-        };
-        addBookToLibrary(); 
-        console.log(myLibrary);
-        myLibrary.forEach(element => {
-            console.log(element);
-            const container = document.querySelector(".container");
-            
-            function createBookDiv (div) {
-            
-                div = document.createElement("div");
-                div.appendChild(document.createTextNode(element))
-                return div
-            }
-            container.appendChild(createBookDiv("div")); 
-        });
-    
-    });
-});
-
-
-// document.getElementById("closeFormButton").addEventListener("click", () => {
-//     console.log("click-test-achieved")
-//     closeForm();
-// });
-
-
-
-
-
-// document.querySelector("button").addEventListener("click", () => {
-//     console.log("click-test-achieved")
-//     const container = document.querySelector(".container");
-    
-//     container.appendChild(createBookDiv("div"));    
-// });
-// document.querySelector("button").addEventListener("click", () => {
-//     console.log("click-test-achieved")
-//     const container = document.querySelector(".container");
-    
-//     container.appendChild(createBookForm("div"));    
-// });
-
-/*
-function myFunction(list){
-    var text = "";
-    var inputs = document.querySelectorAll("input[type=text]");
-    for (var i = 0; i < inputs.length; i++) {
-        text += inputs[i].value;
-    }
-    var li = document.createElement("li");
-    var node = document.createTextNode(text);
-    li.appendChild(node);
-    document.getElementById("list").appendChild(li);
-    */
-/*
-function displayBook (livre) {
-    let div = document.createElement("div");
-    div.style.border = "10px";
-    div.style.bordercolor = "fuschia";
-    div.style.borderstyle = "solid";
-    div.style.width = "50px";
-    div.style.height = "50px";
-    const displayMyLibrary = myLibrary.filter(livre => livre);
-    console.table(displayMyLibrary);
-    return livre;
-
+    document.getElementById("newBookFormDiv").style.display = "none";
 }
-console.table(livre);
-*/
-/*
-const displayMyLibrary = myLibrary.filter(livre => livre);
-console.table(displayMyLibrary);
-const container = document.querySelector("#container");
-container.appendChild(displayBook("div")); */
+document.getElementById("newBookButton").addEventListener("click", () => {
+    openForm();
+    console.log("click-test-achieved")
+    const container = document.querySelector(".container");
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    };
+});
+function addBookToLibrary() { 
+    let authorName = document.getElementById("author").value;
+    alert(authorName);
+    let input = authorName;
+    alert(`You read ${input}`);
+    myLibrary.push(`${input}`);
+    console.log(myLibrary);
+    myLibrary.forEach(element => {
+        console.log(element);
+        const container = document.querySelector(".container");
+        function createBookDiv (div) {
+            div = document.createElement("div");
+            div.appendChild(document.createTextNode(element))
+            return div
+        }
+        container.appendChild(createBookDiv("div")); 
+    });
+};  
+document.getElementById("closeFormButton").addEventListener("click", () => {
+    console.log("click-test-achieved")
+    addBookToLibrary(); 
+    closeForm();
+});
