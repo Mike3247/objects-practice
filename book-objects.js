6b9fba3b0b947b2e5dd31e22a2506cfdeed7a9a5
Mike Selmani <mike_selmani@hotmail.com>
2023-01-25 00:48:24 -0500

    js, refactor object constructor function to a class

--- book-objects.js
+++ book-objects.js
@@ -1,103 +1,103 @@
-function Player (name, marker) {
-    this.name = name
-    this.marker = marker
-    this.sayName = function () {
-        console.log(name)
-    }
+function Player(name, marker) {
+  this.name = name;
+  this.marker = marker;
+  this.sayName = () => {
+    console.log(name);
+  };
 }
-const player1 = new Player ('steve', 'X')
-const player2 = new Player ('also steve', 'O')
-player1.sayName() // logs 'steve'
-player2.sayName() // logs 'also steve'
-console.log('end of tests for this page from here on out.');
+const player1 = new Player("steve", "X");
+const player2 = new Player("also steve", "O");
+player1.sayName(); // logs 'steve'
+player2.sayName(); // logs 'also steve'
+console.log("end of tests for this page from here on out.");
 
-//////////////////////////////////////////////////////////////////
+/// ///////////////////////////////////////////////////////////////
 
-let myLibrary = [/*"The Hobbit", "Harry Potter", "20 000 lieues"*/];
-function Book (title, author, pages, read) {
-    this.title = title.substr(0) + " "
-    this.author = author
-    this.pages = pages
-    this.read = read
+const myLibrary = [/* "The Hobbit", "Harry Potter", "20 000 lieues" */];
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = `${title.substr(0)} `;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
     this.toggleRead = function () {
-        if (read === "Read") {read = "Not Read";
-            return "Not Read"
-        }
-            else if (read === "Not Read") {read = "Read";
-                return "Read";
-        }
-    }
-    this.sayInfo = function () {
-        return "Title: " + title + "Author: " + author +
-         "Number of pages: " + pages + "Read status: "
-    }
-}
-const theHobbit = new Book ("The Hobbit ", "J.R.R. Tolkien ", "295 pages ", "not read yet ")
+      if (read === "Read") {
+        read = "Not Read";
+        return "Not Read";
+      }
+      if (read === "Not Read") {
+        read = "Read";
+        return "Read";
+      }
+    };
+    this.sayInfo = () => `Title: ${title} Author: ${author} Number of pages: ${pages} Read status: `;
+  }
+};
+
+const theHobbit = new Book("The Hobbit ", "J.R.R. Tolkien ", "295 pages ", "not read yet ");
 console.log(theHobbit.sayInfo());
 console.log(theHobbit.constructor);
 const container = document.querySelector(".container");
 const buttonDiv = document.querySelector(".newBookButton");
-const btn = document.querySelector("button")
+const btn = document.querySelector("button");
 function openForm() {
-    document.getElementById("newBookFormDiv").style.display = "block";
-};
+  document.getElementById("newBookFormDiv").style.display = "block";
+}
 function closeForm() {
-    document.getElementById("newBookFormDiv").style.display = "none";
+  document.getElementById("newBookFormDiv").style.display = "none";
 }
 document.getElementById("newBookButton").addEventListener("click", () => {
-    openForm();
-    console.log("click-test-achieved")
+  openForm();
+  console.log("click-test-achieved");
 });
-function addBookToLibrary() { 
-    let title = document.getElementById("title").value;
-    let author = document.getElementById("author").value;
-    let pages = document.getElementById("pages").value;
-    let read = document.querySelector('input[name="readStatus"]:checked').value;
-    alert(read);
-    const additionalBook = new Book (title.substr(0) + " ", author.substr(0) + " ", pages.substr(0) + " ", read);
-    console.log(additionalBook.sayInfo());
-    myLibrary.push(additionalBook.sayInfo());
-    console.log(myLibrary);
-    myLibrary.forEach(element => {
-        console.log(element);
-        const container = document.querySelector(".container");
-        function createBookDiv (div) {
-            div = document.createElement("div");
-            div.appendChild(document.createTextNode(element)) 
-            div.style.border = "solid 10px red";
-            div.dataset.indexNumber = myLibrary.indexOf(additionalBook);
-            const buttonToToggleReadStatus = document.createElement("button");
-            buttonToToggleReadStatus.addEventListener("click", () => {
-                buttonToToggleReadStatus.textContent = additionalBook.toggleRead();
-            });
-            if (read === "Read") {
-                buttonToToggleReadStatus.textContent = "Read";
-            }
-                else if (read === "Not Read") {
-                buttonToToggleReadStatus.textContent = "Not Read";
-            };
-            div.appendChild(buttonToToggleReadStatus);                 
-            const buttonToRemoveBook = document.createElement("button");
-            buttonToRemoveBook.textContent = "Remove Book";
-            div.appendChild(buttonToRemoveBook);
-            buttonToRemoveBook.addEventListener("click", () => {
-                console.log("remove-button-click-test-achieved")
-                container.removeChild(div);
-                myLibrary.splice(div.dataset.indexNumber, 1);
-            })
-            return div
-        }
-        console.log(myLibrary.indexOf(element));
-        container.appendChild(createBookDiv("div")); 
-    });
-};
-document.getElementById("closeFormButton").addEventListener("click", () => {
-    console.log("click-test-achieved")
+function addBookToLibrary() {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const read = document.querySelector("input[name=\"readStatus\"]:checked").value;
+  alert(read);
+  const additionalBook = new Book(`${title.substr(0)}.`, `${author.substr(0)}.`, `${pages.substr(0)}.`, read);
+  console.log(additionalBook.sayInfo());
+  myLibrary.push(additionalBook.sayInfo());
+  console.log(myLibrary);
+  myLibrary.forEach((element) => {
+    console.log(element);
     const container = document.querySelector(".container");
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    };
-    addBookToLibrary(); 
-    closeForm();
+    function createBookDiv(div) {
+      div = document.createElement("div");
+      div.appendChild(document.createTextNode(element));
+      div.style.border = "solid 10px red";
+      div.dataset.indexNumber = myLibrary.indexOf(additionalBook);
+      const buttonToToggleReadStatus = document.createElement("button");
+      buttonToToggleReadStatus.addEventListener("click", () => {
+        buttonToToggleReadStatus.textContent = additionalBook.toggleRead();
+      });
+      if (read === "Read") {
+        buttonToToggleReadStatus.textContent = "Read";
+      } else if (read === "Not Read") {
+        buttonToToggleReadStatus.textContent = "Not Read";
+      }
+      div.appendChild(buttonToToggleReadStatus);
+      const buttonToRemoveBook = document.createElement("button");
+      buttonToRemoveBook.textContent = "Remove Book";
+      div.appendChild(buttonToRemoveBook);
+      buttonToRemoveBook.addEventListener("click", () => {
+        console.log("remove-button-click-test-achieved");
+        container.removeChild(div);
+        myLibrary.splice(div.dataset.indexNumber, 1);
+      });
+      return div;
+    }
+    console.log(myLibrary.indexOf(element));
+    container.appendChild(createBookDiv("div"));
+  });
+}
+document.getElementById("closeFormButton").addEventListener("click", () => {
+  console.log("click-test-achieved");
+  const container = document.querySelector(".container");
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  addBookToLibrary();
+  closeForm();
 });
-
